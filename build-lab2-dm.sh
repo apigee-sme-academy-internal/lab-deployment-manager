3a0fa69742da3dcb971ad3d2b129b88ae48b6381
Miguel Mendoza <miguelmendoza@google.com>
2020-06-18 02:52:24 -0700

    Fix lab build scripts

--- build-lab2-dm.sh
+++ build-lab2-dm.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-export LAB_REPO="git@github.com:apigee-sme-academy-internal/app-modernization-lab-3.git"
+export LAB_ZIP="lab2-dm.zip"
+export LAB_REPO="git@github.com:apigee-sme-academy-internal/app-modernization-lab-2.git"
 export LAB_BRANCH="master"
-
 gcloud secrets versions access --secret=automation-deploy-key latest > ./build/deploy-key.pem
-./build.sh ${LAB_REPO} ${LAB_BRANCH} ./build/deploy-key.pem lab3-dm.zip
+
+
+./build.sh ${LAB_REPO} ${LAB_BRANCH} ./build/deploy-key.pem ${LAB_ZIP}
 rm -f ./build/deploy-key.pem
