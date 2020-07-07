1c77ea9d442135c017981bd164f4e37e881c73dc
Miguel Mendoza <miguelmendoza@google.com>
2020-07-07 11:39:50 -0700

    Add timestamp for DNS creation

--- bin/auth-hook.sh
+++ bin/auth-hook.sh
@@ -7,6 +7,7 @@ source ~/env
 
 export CHALLENGE_PREFIX="_acme-challenge"
 
+add_apigeelabs_dns_entry "TXT" "_created_at.${CERTBOT_DOMAIN}"  "$(date +%s)"
 add_apigeelabs_dns_entry "TXT" "${CHALLENGE_PREFIX}.${CERTBOT_DOMAIN}"  "${CERTBOT_VALIDATION}"
 wait_for_apigeelabs_dns_record "TXT" "${CHALLENGE_PREFIX}.${CERTBOT_DOMAIN}" "${AUTHORITATIVE_NAMESERVER}"
 sleep 15; # sleep extra time for good measure ...
