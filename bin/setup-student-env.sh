2aad9d1ed1c92a23d8049311003f113d52606634
Miguel Mendoza <miguelmendoza@google.com>
2020-07-06 20:36:57 -0700

    Update student env

--- bin/setup-student-env.sh
+++ bin/setup-student-env.sh
@@ -7,12 +7,11 @@ echo "*********************************"
 echo "*** (BEGIN) Setup Student Env ***"
 echo "*********************************"
 
-
 STUDENT_HOME="/home/${QWIKLABS_USERNAME}"
-mkdir -p "$STUDENT_HOME"
-chmod a+rwx "$STUDENT_HOME"
 
 cat << EOF >> $STUDENT_HOME/lab.env
+
+export PATH="/snap/bin:\$PATH"
 
 export PROJECT=\$(gcloud config get-value project)
 
@@ -25,14 +24,13 @@ export ENV="test"
 
 export ACCESS_TOKEN=\$(gcloud auth print-access-token)
 
-export PROJECT_SERVICE_ACCOUNT_JSON='${PROJECT_SERVICE_ACCOUNT_JSON}'
-
 EOF
 
+chmod a+rwx $STUDENT_HOME/lab.env
+
 export HOME=$STUDENT_HOME
-cd ~
-gcloud auth activate-service-account --key-file=<(echo ${PROJECT_SERVICE_ACCOUNT_JSON})
-rm -f /tmp/service_account.json
+sudo -u $QWIKLABS_USERNAME -E bash -c 'gcloud auth activate-service-account --key-file=<(echo ${PROJECT_SERVICE_ACCOUNT_JSON})'
+
 
 echo "*******************************"
 echo "*** (END) Setup Student Env ***"
