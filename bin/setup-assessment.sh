6f1f237523b160044174e27c7d8055345bfc3d32
Miguel Mendoza <miguelmendoza@google.com>
2020-07-15 16:46:33 -0700

    Setup assessment env

--- bin/setup-assessment.sh
+++ bin/setup-assessment.sh
@@ -10,25 +10,15 @@ echo "********************************"
 STUDENT_HOME="/home/${QWIKLABS_USERNAME}"
 export ASSESSMENT_DIR="${STUDENT_HOME}/assessment"
 
-# Copy framework to student's home directory
+# copy framework to student's home directory
 cp -r "${HYBRID_PLAYER_DIR}/assessment" "${ASSESSMENT_DIR}"
 
 cp ~/certs.env "${ASSESSMENT_DIR}/"
 
-# Overwrite environment file
-cat << EOF > "${ASSESSMENT_DIR}/assessment.env"
-# this file is used for activity tracking, do not edit it manually
-BASEDIR="\$( cd "\$( dirname "\${BASH_SOURCE[0]}" )" && pwd )"
-export PROJECT='${PROJECT}'
-export CLUSTER_ZONE='${ZONE}'
-export PATH="/snap/bin:\$PATH"
-export ORG='${PROJECT}'
-export ENV='test'
-export PROJECT_SERVICE_ACCOUNT_JSON='${PROJECT_SERVICE_ACCOUNT_JSON}'
-source "\${BASEDIR}/certs.env"
-EOF
+# render env file
+envsubst < "${ASSESSMENT_DIR}/assessment.env.tpl" > "${ASSESSMENT_DIR}/assessment.env"
 
-# Make files accessible by student
+# make files accessible by student
 chown -R "${QWIKLABS_USERNAME}:ubuntu" "${ASSESSMENT_DIR}"
 
 # Make shell scripts executable
