01340d70a36cb64965fec9ca74657b92222792be
Miguel Mendoza <miguelmendoza@google.com>
2020-07-18 20:49:45 -0700

    Switch to new task tracker and error handler

--- bin/setup-lab-repo.sh
+++ bin/setup-lab-repo.sh
@@ -10,14 +10,20 @@ echo "******************************"
 echo "*** (BEGIN) Lab repo setup ***"
 echo "******************************"
 
-echo '${LAB_PRIVATE_KEY}' > lab-key.pem
+cat << EOF > ~/lab-key.pem
+${LAB_PRIVATE_KEY}
+EOF
+
 chmod 600 ~/lab-key.pem
-export GIT_SSH_COMMAND="ssh -i ~/lab-key.pem"
+export GIT_SSH_COMMAND="ssh -i ${HOME}/lab-key.pem"
+
+ssh-keyscan github.com >> ~/.ssh/known_hosts
 
 clone_repo_and_checkout_branch ${LAB_REPO} ${LAB_BRANCH}
 export LAB_DIR="$(pwd)/$(get_repo_dir ${LAB_REPO})"
 
 cat << EOF >> ~/lab.env
+export GIT_SSH_COMMAND='${GIT_SSH_COMMAND}'
 export LAB_DIR='${LAB_DIR}'
 EOF
 
