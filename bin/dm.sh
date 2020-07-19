ed615156375140a3fdab4fc7464d5dbbecf9b3f6
Miguel Mendoza <miguelmendoza@google.com>
2020-07-18 18:04:36 -0700

    Add simple error handling to DM

--- bin/dm.sh
+++ bin/dm.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/env bash
 
 source ~/env
+
+set -e
+trap 'catch $? $LINENO' EXIT
+catch() {
+  exit_code="$1"
+  exit_line="$2"
+  if [ "$1" != "0" ]; then
+    echo "ERROR: ${exit_code} occurred on ${exit_line}"
+    if ! command -v lab-bootstrap &> /dev/null  ; then
+      lab-bootstrap update overall-deployment "errored"
+      return
+    fi
+  fi
+}
+
 setup_logger "dm"
 
 setup-service-accounts.sh
